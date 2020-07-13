0794c4500eb3b336959eeb6bb806555d50a9e7b2
AlexMartpr <alexmart2001@mail.ru>
2020-07-13 15:29:01 +0300

    added new class

--- sprite.js
+++ sprite.js
@@ -1,3 +1,5 @@
+'use strict'
+import {Anime} from './animations';
 class Sprite{
   constructor({imgName,sX,sY,w=64,h=64}){
     this.imgName=imgName;
@@ -12,4 +14,43 @@ class Sprite{
     this.x=x;
     this.y=y;
   }
+}
+class SpriteSheet {
+  constructor({imgName,imgW,imgH,spriteW=64,spriteH=64}){
+    this.imgName=imgName;
+    this.imgW=imgW;
+    this.imgH=imgH;
+    this.spriteW=spriteW;
+    this.spriteH=spriteH;
+  }
+  getAnime(inds,speed,repeat=true,auto=true){
+    return new Anime({
+      imgName : this.imgName,
+      frames : inds.map(ind => ({
+        sX: this.getSX(ind),
+        sY: this.getSY(ind)
+      })),
+      speed : speed,
+      repeat : repeat,
+      auto : auto,
+      w : this.spriteW,
+      h : this.spriteH
+    });
+  }
+  getSprite(ind){
+    return new Sprite({
+      imgName : this.imgName,
+      sX : this.getSX(ind),
+      sY : this.getSY(ind),
+      w : this.spriteW,
+      h : this.spriteH
+    });
+  }
+  getSX(ind){
+    return (--ind * this.spriteW) % this.imgW;
+  }
+  getSY(ind){
+    return Math.trunc((--ind * this.spriteW) / this.imgW) * this.spriteH;
+  }
+
 }
