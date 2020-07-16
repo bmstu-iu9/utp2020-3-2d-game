5ea0983b69b4b91810ea8efdef2f4fcbb9c4cd0f
AlexMartpr <alexmart2001@mail.ru>
2020-07-16 19:22:30 +0300

    fixed class player(will finish it soon)

--- GamePlayer.js
+++ GamePlayer.js
@@ -1,6 +1,5 @@
 'use strict'
-import {CharacterSheet} from "./sprite"
-export class Vector {
+class Vector {
   constructor(direction,speed){
     this.setDirection(direction, speed);
   }
@@ -14,104 +13,90 @@ export class Vector {
           this.y=-speed;
         break;
        case "Down" :
-          this.y=speed;
+          this.y+=speed;
         break;
        case "Left" :
             this.x=-speed;
           break;
         case "Right" :
-            this.x=speed;
+            this.x+=speed;
           break;
 
     }
   }
 }
-export class ControlState{
-  constructor(){
-    this.up=false;
-    this.down=false;
-    this.left=false;
-    this.right=false;
-    this.fire=false;
-    this.keyMap = new Map([
-      [37 ,'Left'],[38,'Up'],[39,'Right'],[40,'Down']
-    ]);
-    this.fire= new Map([
-      [0,'fireLeft']
-    ]);
-    document.addEventListener('keydown',(event) => this.update(event,true));
-    document.addEventListener('keyup',(event) => this.update(event,false));
-    document.addEventListener('mousedown',(event) => this.updateM(event,true));
-    document.addEventListener('mouseup',(event) => this.updateM(event,false));
-    }
-    update(event,pressed) {
-      if(this.keyMap.has(event.keyCode)){
-        event.preventDefault();
-        event.stopPropagation();
-        this.[this.keyMap.get(event.keyCode)]=pressed;
-      }
-    }
-    updateM(event,pressed){
-      if (this.fire.has(event.button)){
-        event.preventDefault();
-        event.stopPropagation();
-        this.[this.fire.get(event.button)]=pressed;
-      }
-    }
-}
-export class BasicPlayer {
-  constructor({imgName,speed}) {
-    this.x=0;
-    this.y=0;
-    this.speed=speed;
-    this.vec=new Vector("Down",0);
-    this.lastTime=0;
-    this.animations={};
-    this.isFiring=false;
-    const anime = new CharacterSheet({imgName : imgName});
-    "walkDown,walkUp,walkLeft,walkRight".split(",").forEach(name => {
-      this.animations[name] = anime.getAnime(name);
-    })
-  }
-  walk(direction){
-    this.vec.setDirection(direction,this.speed);
-    this.view=this.animations["walk" + direction];
-    this.view.run();
-  }
-  stand(direction){
-    this.vec.setDirection(direction,0);
-    this.view = this.animations("walk" + direction);
-    this.view.stop();
-  }
-  update(time){
-    if (this.lastTime===0){
-      this.lastTime=time;
-      return;
-    }
-    this.x += (time - this.lastTime) * (this.vec.x / 1000);
-    this.y += (time - this.lastTime) * (this.vec.y / 1000);
-    this.lastTime=time;
-    this.view.setXY(Math.trunc(this.x),Math.trunc(this.y));
-    this.view.update(time);
-  }
-}
-export class GamePlayer extends BasicPlayer{
-  constructor(control1,control2){
-    super({imgName :"player",speed:50});
-      this.control1=control1;
-      this.control2=control2;
-    }
-    update(time){
-      if(this.control1.up) {
-            this.walk("Up");
-        } else if(this.control1.down) {
-            this.walk("Down");
-        } else if(this.control1.left) {
-            this.walk("Left");
-        } else if(this.control1.right) {
-            this.walk("Right");
-        } else this.stand(this.velocity.direction);
 
-        super.update(time);
-    }
+class Player {
+  constructor(x, y, speed,sprite) {
+    this.x=x;
+    this.y=y;
+    this.sprite=sprite;
+    this.speed=speed;
+    this.direction="Down";
+    //this.vec=new Vector("Down",0);
+    this.isFiring=false;
+  }
+  move(){
+    if (this.direction == "Down"){
+      this.sprite.down.start();
+      this.sprite.run=true;
+    } else{
+      if (downPressed){
+        this.y+=speed;
+        this.sprite.down.y=this.y;
+        this.sprite.down.start();
+      } else{
+        if (downPressed===false){
+          this.sprite.down.stop();
+        }
+      }
+        if (upPressed){
+          this.y-=speed;
+          this.sprite.up.y=this.y;
+          this.sprite.up.start();
+        } else{
+          if (upPressed===false){
+            this.sprite.up.stop();
+          }
+        }
+          if (rightPressed){
+            this.x+=speed;
+            this.sprite.right.x=this.x;
+            this.sprite.right.start();
+          } else{
+            if (rightPressed===false){
+              this.sprite.right.stop();
+            }
+          } if (leftPressed){
+            this.x-=speed;
+            this.sprite.left.x=x;
+            this.sprite.left.start();
+          } else{
+            if (leftPressed===false){
+              this.sprite.left.stop();
+            }
+          }
+          }
+        }
+  //walk(direction){
+    //this.vec.setDirection(direction,this.speed);
+    //this.view=this.animations["walk" + direction];
+    //this.view.run();
+  //}
+  //stand(direction){
+    //this.vec.setDirection(direction,0);
+    //this.view = this.animations("walk" + direction);
+    //this.view.stop();
+  //}
+  //update(time){
+    //if (this.lastTime===0){
+      //this.lastTime=time;
+      //return;
+    //}
+    //this.x += (time - this.lastTime) * (this.vec.x / 1000);
+    //this.y += (time - this.lastTime) * (this.vec.y / 1000);
+    //this.lastTime=time;
+    //this.view.setXY(Math.trunc(this.x),Math.trunc(this.y));
+    //this.view.update(time);
+  //}
 }
