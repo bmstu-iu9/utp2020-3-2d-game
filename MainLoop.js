7e7651a8c7cf4f81ff47b6d5f7b732040d0c12e7
AlexMartpr <alexmart2001@mail.ru>
2020-07-19 15:35:15 +0300

    Nothing

--- MainLoop.js
+++ MainLoop.js
@@ -10,12 +10,22 @@ let cameraStartY = 20;
 
 let sightWidth = 8;
 let sightHeight = 2;
+const im=new Image();
+im.src="player.png";
+var sprite = {
+  up : new Sprite(im,0,64*8,64,64,100,100,8),
+  down : new Sprite(im,0,64*10,64,64,100,100,10),
+  left : new Sprite(im,0,64*9,64,64,100,100,9),
+  right : new Sprite(im,0,64*11,64,64,100,100,11),
+};
+var pl=new Player(100,100,64,64,1, sprite);
 
 const map = new Image();
 map.src = "map.png";
 
 const images = {};
 images["map"] = map;
+images["player"] = im;
 
 const camera = new Camera(cameraStartX, cameraStartY, map);
 const sight = new Sight(canvas.width, canvas.height, sightWidth, sightHeight);
@@ -29,6 +39,7 @@ const canvasToWorld = (x, y) => {
 }
 
 const update = () => {
+  pl.move();
   camera.updateCoordinates();
 
 
@@ -37,6 +48,7 @@ const update = () => {
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   camera.drawVisibleMap();
+  pl.drawDirection();
   sight.draw();
 
 }
