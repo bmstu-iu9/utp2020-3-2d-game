ed4a6ab8e2e92f335cede7ba20fe8bfb95d51b22
AlexMartpr <alexmart2001@mail.ru>
2020-07-19 15:38:55 +0300

    Fixed class

--- GamePlayer.js
+++ GamePlayer.js
@@ -1,50 +1,24 @@
 'use strict'
-class Vector {
-  constructor(direction,speed){
-    this.setDirection(direction, speed);
-  }
-  setDirection(direction,speed){
-    this.direction=direction;
-    this.speed=speed;
-    this.x=0;
-    this.y=0;
-    switch (direction) {
-      case "Up":
-          this.y=-speed;
-        break;
-       case "Down" :
-          this.y+=speed;
-        break;
-       case "Left" :
-            this.x=-speed;
-          break;
-        case "Right" :
-            this.x+=speed;
-          break;
 
-    }
-  }
-}
-var pl=new Player(100,100,0.5,
-sprite{
-  up : new Sprite(gpImage,0,64*8,64,64,pl.x,pl.y,8),
-  down : new Sprite(gpImage,0,64*10,64,64,pl.x,pl.y,10),
-  left : new Sprite(gpImage,0,64*9,64,64,pl.x,pl.y,9),
-  right : new Sprite(gpImage,0,64*11,64,64,pl.x,pl.y,11),
-})
 class Player {
-  constructor(x, y, speed,sprite) {
+
+  constructor (x, y,width,height, speed,sprite) {
+
     this.x=x;
     this.y=y;
+    this.w=width;
+    this.h=height;
     this.sprite=sprite;
     this.speed=speed;
     this.direction="Down";
-    //this.vec=new Vector("Down",0);
     this.isFiring=false;
+
   }
-  move(){
-    if (this.direction == "Down"){
-      ctx.drawImage(
+
+  drawDirection() {
+
+    if (this.direction == "Down") {
+      ctx.drawImage (
           this.sprite.down.image,
           this.sprite.down.srcX,
           this.sprite.down.srcY,
@@ -55,69 +29,84 @@ class Player {
           this.sprite.down.width,
           this.sprite.down.height
       );
-      //this.sprite.down.start();
-      //this.sprite.run=true;
-    } else{
-      if (downPressed){
-        this.y+=speed;
+    }
+
+    if (this.direction == "Up") {
+      ctx.drawImage (
+          this.sprite.up.image,
+          this.sprite.up.srcX,
+          this.sprite.up.srcY,
+          this.sprite.up.width,
+          this.sprite.up.height,
+          this.x,
+          this.y,
+          this.sprite.up.width,
+          this.sprite.up.height
+      );
+    }
+
+    if (this.direction == "Left") {
+      ctx.drawImage (
+          this.sprite.left.image,
+          this.sprite.left.srcX,
+          this.sprite.left.srcY,
+          this.sprite.left.width,
+          this.sprite.left.height,
+          this.x,
+          this.y,
+          this.sprite.left.width,
+          this.sprite.left.height
+      );
+    }
+
+    if (this.direction == "Right") {
+      ctx.drawImage (
+          this.sprite.right.image,
+          this.sprite.right.srcX,
+          this.sprite.right.srcY,
+          this.sprite.right.width,
+          this.sprite.right.height,
+          this.x,
+          this.y,
+          this.sprite.right.width,
+          this.sprite.right.height
+      );
+    }
+
+  }
+
+  move() {
+
+      if (downPressed) {
+        this.y+=this.speed;
+        this.sprite.down.x=this.x;
         this.sprite.down.y=this.y;
-        if (this.sprite.down.run == true) this.sprite.down.run=false;
-        this.sprite.down.start();
-      } else{
-        if (downPressed===false){
-          this.sprite.down.stop();
+        this.direction = "Down";
+        this.sprite.down.drawSprite();
+      } else {
+        if (upPressed) {
+          this.y-=this.speed;
+          this.sprite.up.x=this.x;
+          this.sprite.up.y=this.y;
+          this.direction = "Up";
+          this.sprite.up.drawSprite();
         }
       }
-        if (upPressed){
-          this.y-=speed;
-          this.sprite.up.y=this.y;
-          if (this.sprite.up.run == true) this.sprite.up.run=false;
-          this.sprite.up.start();
-        } else{
-          if (upPressed===false){
-            this.sprite.up.stop();
-          }
-        }
-          if (rightPressed){
-            this.x+=speed;
+
+      if (rightPressed) {
+            this.x+=this.speed;
             this.sprite.right.x=this.x;
-            if (this.sprite.right.run == true) this.sprite.right.run=false;
-            this.sprite.right.start();
-          } else{
-            if (rightPressed===false){
-              this.sprite.right.stop();
-            }
-          } if (leftPressed){
-            this.x-=speed;
-            this.sprite.left.x=x;
-            if (this.sprite.left.run == true) this.sprite.left.run=false;
-            this.sprite.left.start();
-          } else{
-            if (leftPressed===false){
-              this.sprite.left.stop();
+            this.sprite.right.y=this.y;
+            this.direction = "Right";
+            this.sprite.right.drawSprite();
+          } else {
+          if (leftPressed) {
+            this.x-=this.speed;
+            this.sprite.left.x=this.x;
+            this.sprite.left.y=this.y;
+            this.direction = "Left";
+            this.sprite.left.drawSprite();
             }
           }
-          }
-        }
-  //walk(direction){
-    //this.vec.setDirection(direction,this.speed);
-    //this.view=this.animations["walk" + direction];
-    //this.view.run();
-  //}
-  //stand(direction){
-    //this.vec.setDirection(direction,0);
-    //this.view = this.animations("walk" + direction);
-    //this.view.stop();
-  //}
-  //update(time){
-    //if (this.lastTime===0){
-      //this.lastTime=time;
-      //return;
-    //}
-    //this.x += (time - this.lastTime) * (this.vec.x / 1000);
-    //this.y += (time - this.lastTime) * (this.vec.y / 1000);
-    //this.lastTime=time;
-    //this.view.setXY(Math.trunc(this.x),Math.trunc(this.y));
-    //this.view.update(time);
-  //}
+    }
 }
