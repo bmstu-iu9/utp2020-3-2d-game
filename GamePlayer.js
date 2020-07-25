4736467a4a9e78afb6402bc6e6f6c181710d7863
AlexMartpr <alexmart2001@mail.ru>
2020-07-25 16:25:41 +0300

    add coordinates of center of player and processing shots

--- GamePlayer.js
+++ GamePlayer.js
@@ -7,6 +7,9 @@ class Player {
     this.y = y;
     this.w = width;
     this.h = height;
+    this.X_Center = this.x + this.width / 2;
+    this.Y_Center = this.y + this.height / 2;
+    this.radius = (this.width / 2) + 1;
     this.sprite = sprite;
     this.speed = speed;
     this.direction = "Down";
@@ -37,6 +40,7 @@ class Player {
     if (downPressed) {
       if (this.y < mapImg.naturalHeight) {
         this.y += this.speed;
+        this.Y_Center += this.speed;
       }
       this.sprite.down.x = worldToCanvas(this.x - spriteWKoef, 0);
       this.sprite.down.y = worldToCanvas(this.y - spriteHKoef, 1);
@@ -45,6 +49,7 @@ class Player {
     } else if (upPressed) {
       if (this.y !== 0) {
         this.y -= this.speed;
+        this.Y_Center -= this.speed;
       }
       this.sprite.up.x = worldToCanvas(this.x - spriteWKoef, 0);
       this.sprite.up.y = worldToCanvas(this.y - spriteHKoef, 1);
@@ -55,6 +60,7 @@ class Player {
     if (rightPressed) {
       if (this.x < mapImg.naturalWidth) {
         this.x += this.speed;
+        this.X_Center += this.speed;
       }
       this.sprite.right.x = worldToCanvas(this.x - spriteWKoef, 0);
       this.sprite.right.y = worldToCanvas(this.y - spriteHKoef, 1);
@@ -63,11 +69,41 @@ class Player {
     } else if (leftPressed) {
       if (this.x !== 0) {
         this.x -= this.speed;
+        this.X_Center -= this.speed;
       }
       this.sprite.left.x = worldToCanvas(this.x - spriteWKoef, 0);
       this.sprite.left.y = worldToCanvas(this.y - spriteHKoef, 1);
       this.direction = "Left";
       this.sprite.left.update();
     }
+
+    if (mouseDown) {
+      if (shootEnable) {
+        if (player.bulletsInMagazine === 0) {
+          this.reload = true;
+          if (player.magazine !== 0) {
+            player.bulletsInMagazine = 30;
+            player.magazine--;
+            this.reload = false;
+          } else {
+            this.reload = false;
+          }
+        } else {
+          player.bulletsInMagazine--;
+          bullets.add(new Bullet(player.x, player.y,
+                                 canvasToWorld(sight.x, 0), canvasToWorld(sight.y, 1),
+                                 bulletSpeed));
+          if (singleShoot) {
+            shootEnable = false;
+          }
+        }
+      }
+    }
+
   }
+
+  checkBullets() {
+    return player.bulletsInMagazine !== 0 || player.magazine !== 0;
+  }
+
 }
