6d063a704c61a4ae84ae2b74112d839dba803754
frussian <zoomod57@gmail.com>
2020-07-27 13:29:34 +0300

    fix #8 and little bullet refactoring

--- Bullet.js
+++ Bullet.js
@@ -23,7 +23,7 @@ class Bullet {
 
   drawRandomFire() {
     let bulletStep = 0.3;
-    
+
     for (let pxY = this.y - this.bulletAnimationRadius; pxY < this.y + this.bulletAnimationRadius; pxY += bulletStep) {
       for (let pxX = this.x - this.bulletAnimationRadius; pxX < this.x + this.bulletAnimationRadius; pxX += bulletStep){
         let dist = Math.sqrt(Math.pow(pxX - this.x, 2) + Math.pow(pxY - this.y, 2));
@@ -60,18 +60,20 @@ class Bullet {
     let mainStep = 1/4;
     let normStep = 0.3;
     let gap = 0.8;
+    let mainLen = this.speed * gap; //поменять gap, чтобы увеличить расстояние
+    let iterateLen = 0;
 
-    while (Math.abs(dx) < Math.abs(this.dx * gap) &&  //поменять gap, чтобы увеличить расстояние
-           Math.abs(dy) < Math.abs(this.dy * gap)) {
-      if (Math.abs(dx) > Math.abs(this.dx * gap * 0.3)) maxLen = 0.4;
-      if (Math.abs(dx) > Math.abs(this.dx * gap * 0.5)) maxLen = 0.6;
-      if (Math.abs(dx) > Math.abs(this.dx * gap * 0.7)) maxLen = 0.9;
+    while (iterateLen < mainLen){
+
+      if (iterateLen > mainLen * 0.3) maxLen = 0.4;
+      if (iterateLen > mainLen * 0.5) maxLen = 0.6;
+      if (iterateLen > mainLen * 0.7) maxLen = 0.9;
 
       let normLen1 = Math.max(0, maxLen - Math.random());
       let normLen2 = Math.max(0, maxLen - Math.random());
 
-      for (let pxX = 0, pxY = 0; Math.abs(pxX) < Math.abs(norm1Dx * normLen1) && Math.abs(pxY) < Math.abs(norm1Dy * normLen1);
-           pxX += norm1Dx * normStep, pxY += norm1Dy * normStep) {
+      for (let pxX = 0, pxY = 0, iterateNorm1Len = 0; iterateNorm1Len < normLen1;
+           pxX += norm1Dx * normStep, pxY += norm1Dy * normStep, iterateNorm1Len += normStep) {
           ctx.beginPath();
           ctx.rect(worldToCanvas(this.x + pxX + dx, 0), worldToCanvas(this.y + pxY + dy, 1), 1, 1);
           ctx.fillStyle = "yellow";
@@ -82,8 +84,8 @@ class Bullet {
           ctx.closePath();
       }
 
-      for (let pxX = 0, pxY = 0; Math.abs(pxX) < Math.abs(norm2Dx * normLen2) && Math.abs(pxY) < Math.abs(norm2Dy * normLen2);
-           pxX += norm2Dx * normStep, pxY += norm2Dy * normStep) {
+      for (let pxX = 0, pxY = 0, iterateNorm2Len = 0; iterateNorm2Len < normLen2;
+           pxX += norm2Dx * normStep, pxY += norm2Dy * normStep, iterateNorm2Len += normStep) {
           ctx.beginPath();
           ctx.rect(worldToCanvas(this.x + pxX + dx, 0), worldToCanvas(this.y + pxY + dy, 1), 1, 1);
           ctx.fillStyle = "yellow";
@@ -95,6 +97,7 @@ class Bullet {
       }
       dx += unitDx * mainStep;
       dy += unitDy * mainStep;
+      iterateLen += mainStep;
     }
   }
 
