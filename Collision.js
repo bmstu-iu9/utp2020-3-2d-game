b8c583cf874c502c20b28bd922976f69c18cce2b
frussian <zoomod57@gmail.com>
2020-08-05 17:35:28 +0300

    Sequence of checks fix #9

--- Collision.js
+++ Collision.js
@@ -12,28 +12,37 @@ const collision = () => {
   for (let bul of bullets) {
     let f = false;
 
-    for (let i = 0; i < 5; i++) {
-      if (bul.bulletRadius + player.radius >= dist(bul.x + (bul.dx * i) / 5, player.X_Center, bul.y + (bul.dy * i) / 5, player.Y_Center)) {
-          f = true;
+    if (bul.x < 0 || bul.x >= images["map"].naturalWidth ||
+        bul.y < 0 || bul.y >= images["map"].naturalHeight) {
+      f = true;
+    }
+
+    if (!f) {
+      for (let i = 0; i < 5; i++) {
+        if (bul.bulletRadius + player.radius >= dist(bul.x + (bul.dx * i) / 5, player.X_Center, bul.y + (bul.dy * i) / 5, player.Y_Center)) {
+            f = true;
+        }
       }
     }
 
-    if (bul.justShooted == false) {
-      for (let i = 0; i < targets.length; i++) {
-        for (let j = 0; j < 5; j++) {
-          let x1 = bul.x + (bul.dx * j) / 5;
-          let y1 = bul.y + (bul.dy * j) / 5;
-          if (bul.bulletRadius + targets[i].r >= dist(x1, targets[i].x, y1, targets[i].y) &&
-              targets[i].shooted == true) {
-            f = true;
-            targets[i].shooted = false;
+    if (!f) {
+      if (bul.justShooted == false) {
+        for (let i = 0; i < targets.length; i++) {
+          for (let j = 0; j < 5; j++) {
+            let x1 = bul.x + (bul.dx * j) / 5;
+            let y1 = bul.y + (bul.dy * j) / 5;
+            if (bul.bulletRadius + targets[i].r >= dist(x1, targets[i].x, y1, targets[i].y) &&
+                targets[i].shooted == true) {
+              f = true;
+              targets[i].shooted = false;
+            }
+            if (f) {
+              break;
+            }
           }
           if (f) {
             break;
           }
-        }
-        if (f) {
-          break;
         }
       }
     }
@@ -44,11 +53,6 @@ const collision = () => {
       if ((tileMap[yBlock][xBlock] == "black") || (tileMap[yBlock][xBlock] == "cover")) {
         f = true;
       }
-    }
-
-    if (bul.x < 0 || bul.x >= images["map"].naturalWidth ||
-        bul.y < 0 || bul.y >= images["map"].naturalHeight) {
-      f = true;
     }
 
     if (f) {
